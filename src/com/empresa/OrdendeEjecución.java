1b5abbbeff7b30471ec883c02caab8a5ac31d8b7
Palacios <bibiana.palacios@softwareone.com>
2025-11-20 20:27:37 -0500

    second commit 04-08-2025

---
+++ src/com/empresa/OrdendeEjecución.java
@@ -0,0 +1,20 @@
+package com.empresa;
+
+public class OrdendeEjecución {
+
+	public static void main(String[] args) {
+		String var1 = "Hola";
+		String var2 = "Equipo";
+		
+		int a = 100;
+		int b = 200;
+		
+		System.out.print(var1+" "+var2);		
+		System.out.print(a+" "+b);		
+		System.out.print(a+" "+b+" "+var1+" "+var2);
+		System.out.print(var1+" "+var2+" "+a+" "+b);
+		
+
+	}
+
+}
