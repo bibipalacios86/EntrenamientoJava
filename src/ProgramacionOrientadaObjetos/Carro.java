4ceca570c06cbd3c8390db75e41aa0e90427f1e5
Palacios <bibiana.palacios@softwareone.com>
2026-02-04 11:52:38 -0500

    commit 4-2-2026 se crea paquete con las clases de herencia

---
+++ src/ProgramacionOrientadaObjetos/Carro.java
@@ -0,0 +1,8 @@
+package ProgramacionOrientadaObjetos;
+
+public class Carro {
+public void run()
+{
+	System.out.println("El carro está moviendose");
+}
+}
---
+++ src/ProgramacionOrientadaObjetos/Mazda.java
@@ -0,0 +1,12 @@
+package ProgramacionOrientadaObjetos;
+
+public class Mazda extends Carro {
+	static String nombre="mazda";
+	
+	public static void main(String[]args) {
+		Carro car = new Carro();
+		System.out.println(nombre);
+		car.run();
+	}
+
+}
